0bb0fdc99b979a7df99abc326b3dc1e8eb56f640
Carrie Mathieu <carrieburks@gmail.com>
2020-08-14 00:42:40 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+  
+  def initialization(word)
+    @word = word
+  end
+  
+  def match(word)
+    word = word.split("")
+  #loop through each character via word.split("")
+  #if all character matches (array1.sort == word.sort), keep going
+  #if one character doesn't match, delete from array
+  end
+end
